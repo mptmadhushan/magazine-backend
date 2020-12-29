ad606e1c6f4d2c015b127cac9482df0c483462b9
mptmadhushan <tharindumadhushan420@outlook.com>
2020-12-30 03:12:19 +0530

    adede f

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ require("./app/routes/posts.routes")(app);
 require("./app/routes/ads.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 8085;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
